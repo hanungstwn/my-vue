b37ab19a7e1af383b3af5e377c7fb69e1cbf2dc2
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2023-08-09 14:33:00 +0700

    add workflow and deployment config

---
+++ create_img.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+IMAGE_NAME="vue-inputer"
+
+if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
+    docker compose down && docker rmi vue-inputer && docker build -t vue-inputer .
+else
+    docker build -t vue-inputer . 
+fi
+
+
